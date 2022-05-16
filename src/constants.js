04c2b5d89b945b1f162f5f6a9e17f9e62b21c9b7
m.hoseinzadeh <mr.hossenzadeh@gmail.com>
2022-05-16 21:29:29 +0430

    change url whatsapp fom api to web

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const URL_ASSETS_LOGOS = 'https://raw.githubusercontent.com/ktquez/vue-so
 export const URL_ASSETS_ICONS = 'https://raw.githubusercontent.com/ktquez/vue-social-chat/master/src/icons'
 
 export const HREF_BY_APP = {
-  whatsapp: 'https://api.whatsapp.com/send?phone=%ph%&text=',
+  whatsapp: 'https://web.whatsapp.com/send?phone=%ph%&text=',
   messenger: 'https://m.me/%ph%',
   telegram: 'https://telegram.me/%ph%',
   viber: 'viber://chat?number=%ph%',
